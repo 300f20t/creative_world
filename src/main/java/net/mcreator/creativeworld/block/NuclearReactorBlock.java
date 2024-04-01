dbe658cb41c1796023a41a71ede439b779daba4c
BuildTools <93136063+300f20t@users.noreply.github.com>
2024-04-01 23:07:04 +0300

    add nuuuuuke

--- src/main/java/net/mcreator/creativeworld/block/NuclearReactorBlock.java
+++ src/main/java/net/mcreator/creativeworld/block/NuclearReactorBlock.java
@@ -6,19 +6,21 @@ import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import java.util.List;
 import java.util.Collections;
 
-public class NuclearReactorBlock extends Block {
+public class NuclearReactorBlock extends FallingBlock {
 	public NuclearReactorBlock() {
-		super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.GRAVEL).strength(1f, 10f));
+		super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.METAL).strength(1f, 10f).friction(1f));
 	}
 
 	@Override
@@ -32,10 +34,20 @@ public class NuclearReactorBlock extends Block {
 	}
 
 	@Override
+	public boolean isSignalSource(BlockState state) {
+		return true;
+	}
+
+	@Override
+	public int getSignal(BlockState blockstate, BlockGetter blockAccess, BlockPos pos, Direction direction) {
+		return 5;
+	}
+
+	@Override
 	public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
+		return Collections.singletonList(new ItemStack(Blocks.ACACIA_PLANKS, 2));
 	}
 }
